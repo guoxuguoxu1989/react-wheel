fc39a5a1fe64e8e0f09557e10cdd4fdd23a023c6
guoxu1989 <guoxuguoxu1989@gmail.com>
2021-03-18 20:31:40 +0800

    添加onclick事件

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,7 +2,9 @@ import Icon from './icon';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+const fn = () => {
+  console.log('fn');
+};
 ReactDOM.render(<div>
-  <Icon name='wechat' />
-  <Icon name="alipay" />
+  <Icon name="alipay" onClick={fn} />
 </div>, document.querySelector('#root'));
